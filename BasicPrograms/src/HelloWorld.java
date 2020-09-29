ae0dde6d99496a286e07b96305ac07c5cea9cd67
c-shanan <45417268+nandisharmistha@users.noreply.github.com>
2020-09-30 00:02:55 +0530

    first code

---
+++ BasicPrograms/src/HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+
+    static int x = 4;
+
+    public static void main(String[] args)
+    { //the main block is the main execution entry point of a
+        // Java Program
+        System.out.println("Hello World!");
+        System.out.println("My name is Sharmistha!"); //prints the next statement on the next line
+        System.out.print("How are you?");
+        System.out.print(" Glad to meet you!\n"); //does not print on the next line
+        System.out.println(x);
+    }
+}
